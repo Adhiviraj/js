54877c6a6dda575a016f1948af79a78b571c28a4
Adhiviraj <paladhiviraj@gmail.com>
2026-06-02 22:50:40 +0530

    data type summery

---
+++ basic/04_comparison.js
@@ -0,0 +1,36 @@
+// <-- Basic int to integer -->
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 % 2)
+// console.log(2 / 2)
+
+// <-- String to integer -->
+
+// console.log("2" + 1)
+// console.log("2" + "1")
+// console.log(2 + "1")
+// console.log("4" - 1)
+
+
+// <-- string comparision -->
+
+// console.log("2" < 1)
+// console.log("2" > 1)
+// console.log("2" >= 1)
+
+// <-- null Comparison --> 
+
+// console.log(null > 0); // unpredictable given by java script
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+
+// <-- strict check "===" -->
+
+console.log( "2" === 2);
---
+++ basic/05_datatypesSummary.js
@@ -0,0 +1,13 @@
+// <<-- Primitive -->> pass by value
+
+// 7 types:-
+// Number, String , Boolean , null , undefined ,  Symbols , BigInt
+
+// <<-- Non Primitive -->> pass by reference
+//  Also known as Reference
+
+// Array , Object , Function
+
+// JavaScript is dynamically typed language
+
+// Symbols
