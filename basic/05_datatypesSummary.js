ef09b462831ae021dfa543c060c87ab6c71364ec
Adhiviraj <paladhiviraj@gmail.com>
2026-06-03 20:49:12 +0530

    Memory Types(Heap, Stack)

--- basic/05_datatypesSummary.js
+++ basic/05_datatypesSummary.js
@@ -11,3 +11,12 @@
 // JavaScript is dynamically typed language
 
 // Symbols
+
+// <-- Memory types -->
+
+// two main types of memory :-
+// Stack (used in primitive data types )
+// Heap (used in Non-primitive data types)
+// In Stack memory we get the copy of origin memory 
+// In Heap memory we get the referance of the origin memory
+// get the origin memory as it is.
