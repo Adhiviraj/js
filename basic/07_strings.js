03a28cddc40a894ba40f3c9037f7b051ad96b8ac
Adhiviraj <paladhiviraj@gmail.com>
2026-06-03 22:42:46 +0530

    String

---
+++ basic/07_strings.js
@@ -0,0 +1,16 @@
+const name = "Adhiviraj"
+const RepoCount = 5
+
+// console.log(name + " " + RepoCount)
+// This is very old way to merge strings not user friendly.
+
+// For that we use "string Interpolation"
+// In this we use place holder 
+
+console.log(`Hello my name is ${name} and my repo count is ${RepoCount}`)
+
+// differnet way of initializing the strings
+
+const gameName = new String('Adhiviraj')
+console.log(gameName) //for this method the String is stored in key value pair 
+// data types will be object
