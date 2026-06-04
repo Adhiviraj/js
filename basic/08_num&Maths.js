e91e7ccfdd000626f593354816e8753e18cc781d
Adhiviraj <paladhiviraj@gmail.com>
2026-06-04 12:12:31 +0530

    Math in js

---
+++ basic/08_num&Maths.js
@@ -0,0 +1,16 @@
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.343))
+// console.log(Math.ceil(4.243))
+// console.log(Math.floor(4.943))
+
+console.log(Math.random())
+// this value will all way comes between 0 to 1;
+
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
