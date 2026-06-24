5038e0186e32277b75640912e7f8131c8f398530
Adhiviraj <paladhiviraj@gmail.com>
2026-06-24 10:53:22 +0530

    Objects destructuring and json api

---
+++ 02_basic/05_Obj_Destructuring.js
@@ -0,0 +1,23 @@
+const course = {
+    courseName: "js by chai or code",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor:Instructor} = course
+// we can change the name also like Instructor
+console.log(courseInstructor);
+
+// {
+//     "name": "Adhiviraj",
+//     "courseName": "Js hindi",
+//     "Price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
