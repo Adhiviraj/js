ac7a4daf2e5186442e2cafb33eb92ef68ad67aca
Adhiviraj <paladhiviraj@gmail.com>
2026-06-25 14:40:12 +0530

    basic loops

---
+++ 05_iteration/01_loops.js
@@ -0,0 +1,34 @@
+// for
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(i);
+// }
+
+
+// break and continue
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(i);
+//     if(i == 7){
+//         break;
+//     }
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if(i == 7){
+//         continue;
+//     }
+//     console.log(i);
+//     if(i == 7){
+//         break;
+//     }
+// }
+
+// While and do while loop
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+} while(score <= 10);
